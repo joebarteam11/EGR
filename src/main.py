a984c20993672d5f7bd90976408d0c86bf82adf2
theot <85874120+joebarteam11@users.noreply.github.com>
2023-06-08 16:36:25 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -11,9 +11,9 @@ if __name__ == '__main__':
 
     # get the start time
     st = time.time()
-    temptlist = [1700]#[i for i in np.arange(300,501,50)]
-    presslist = [10e5,18e5]#[i for i in np.arange(1e5,5.1e5,5e4)]
-    config = case('H2:1.',                     #fuel compo
+    temptlist = [300]#[i for i in np.arange(300,501,50)]
+    presslist = [1e5,5e5] #[i for i in np.arange(1e5,5.1e5,5e4)]
+    config = case('CH4:1.',                     #fuel compo
                   temptlist,                    #tin fuel
                   presslist,                        #pin fuel
                   'O2:1. N2:3.76',              #ox compo
@@ -22,30 +22,13 @@ if __name__ == '__main__':
                   'CO2:1.',                     #egr compo
                   temptlist,                    #tin egr
                   presslist,                        #pin egr
-                  [0],#[i for i in np.arange(0.60,2.51,0.1)],        #phi range
-                  [i for i in np.linspace(0.0,0.6,30)],#[0.0,0.1,0.15,0.2],            #egr range
-                  'mole',                       #egr rate unit
-                  'schemes/Aramco13.cti',               #scheme
+                  [0.8,1.0,1.2],#[i for i in np.arange(0.60,2.51,0.1)],        #phi range
+                  [0],#[i for i in np.linspace(0.0,0.6,30)],#[0.0,0.1,0.15,0.2],            #egr range
+                  'mole',                       #egr rate unit mole / mass
+                  'gri30.cti',               #path to scheme
                   'Mix', #transport model
-                  'NA'  #is an ARC chemistry ? 'ARC' = yes, other = no
+                  'NA',  #is an ARC chemistry ? 'ARC' = yes, other = no
                  )
-    # temptlist = [i for i in np.arange(300,500,5)]
-    # config = case('CH4:1.',                     #fuel compo
-    #               temptlist,                    #tin fuel
-    #               [1e5,2e5,3e5],                        #pin fuel
-    #               'O2:1. N2:3.76',              #ox compo
-    #               temptlist,                    #tin ox
-    #               [1e5,2e5,3e5],                        #pin ox
-    #               'CO2:1.',                     #egr compo
-    #               temptlist,                    #tin egr
-    #               [1e5,2e5,3e5],                        #pin egr
-    #               [0.8,1,1.2],#[i for i in np.arange(0.80,1.22,0.05)],        #phi range
-    #               [0.1,0.15,0.2],            #egr range
-    #               'mole',                       #egr rate unit
-    #               'gri30.cti',               #scheme
-    #               'Mix', #transport model
-    #               'NA'  #is an ARC chemistry ? 'ARC' = yes, other = no
-    #              )
 
     Tins = [[t[i] for t in config.tin] for i in range(len(config.tin.fuel))]
     Pins = [[p[i] for p in config.pin] for i in range(len(config.pin.fuel))]
@@ -62,9 +45,8 @@ if __name__ == '__main__':
     def update(*a):
         pbar.update()
 
-    dim='equilibrate'
-    #species = ['H2','O2','N2','H2O']
-    species = ['O2','CO','CO2']
+    dim='1D'
+
 
     if(dim=='equilibrate'):
         #species = ['O2','CO','CO2']
@@ -109,11 +91,12 @@ if __name__ == '__main__':
     elif(dim=='1D'):
         real_egr = False
         restart_rate = None # config.egr_range[0] #set to None if want to restart computation from the first egr value in egr_range
-        
+        varstosave = ['phi','AF','P','Tin','T','u','dF']
+        species = ['CH4','H2','O2','CO','CO2','H2O']
 
         #Computation pool 
         pool = mp.Pool(min(len(items),ncpu))
-        results = [pool.apply_async(compute_solutions_1D, args=item+[restart_rate,real_egr,species],callback=update) for item in items]
+        results = [pool.apply_async(compute_solutions_1D, args=item+[restart_rate,real_egr,species,varstosave],callback=update) for item in items]
         pool.close()
         # wait for all tasks to complete and processes to close
         pool.join()
