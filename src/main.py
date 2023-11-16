e5da94f9757a71cb9b1149aeedd5630beea9e7d5
theot <85874120+joebarteam11@users.noreply.github.com>
2023-11-16 17:43:30 +0100

    Cleaning main

--- src/main.py
+++ src/main.py
@@ -40,27 +40,29 @@ if __name__ == '__main__':
         templistFuel = [393] #[i for i in np.arange(290,305,10.0)]
         templistEGR = templistOx
         presslist= [5.01325E5] #[i for i in np.arange(1E5,1.4E5,0.2E5)]
-        phirange =  [i for i in np.arange(0.605,1.206,0.1)]#[0.85,0.1] # [0.6] [0.6,0.7,0.8,0.9,1.0,1.05,1.1005,1.2005,1.3005,1.4005]#
+        phirange =  [i for i in np.arange(0.605,1.206,0.1)] # [0.85,0.1] # [0.6] [0.6,0.7,0.8,0.9,1.0,1.05,1.1005,1.2005,1.3005,1.4005]#
+        # fuelblendrange = [0.0] if no fuel blend needed
         fuelblendrange = [0.0]#[i for i in np.arange(0.0,0.301,0.100)] # 
-        egrrange = [0.1]#[i for i in np.arange(0.0,0.301,0.1)]
-        config = case(['CH4:1.0','H2:1.0'],         #fuel compo
-                    templistFuel,                    #tin fuel
+        # egrrange = [0.0] if no dilution needed
+        egrrange = [0.2]#[i for i in np.arange(0.0,0.301,0.1)] 
+        config = case(['CH4:1.0','H2:1.0'],       #fuel compo   e.g. ['CH4:1.0','H2:1.0'] with fuel blend = [0.0] is equivalent to pure CH4 as fuel, with fuel blend = [0.1] is equivalent to 90% CH4 and 10% H2
+                    templistFuel,                 #tin fuel
                     presslist,                    #pin fuel
-                    'O2:1.0 N2:3.76',              #ox compo
-                    templistOx,                    #tin ox
+                    'O2:1.0 N2:3.76',             #ox compo
+                    templistOx,                   #tin ox
                     presslist,                    #pin ox
-                    'CO2:1.0',                     #egr compo
-                    templistEGR,                    #tin egr
+                    'CO2:1.0',                    #egr compo
+                    templistEGR,                  #tin egr
                     presslist,                    #pin egr
                     phirange,                     #[i for i in np.arange(0.60,2.51,0.1)],        #phi range
-                    fuelblendrange,
+                    fuelblendrange,               #fuelblend 
                     egrrange,                     #[i for i in np.linspace(0.0,0.6,30)],#[0.0,0.1,0.15,0.2],            #egr range
                     'mole',                       #fuelblend rate unit mole / mass
                     'mole',                       #egr rate unit mole / mass
-                    #path+'schemes/CH4_15_256_9_AP.cti',#'gri30.cti',
-                    'gri30.cti',
+                    path+'schemes/CH4_15_256_9_AP.cti',  #fuel mechanism
+                    #'gri30.cti',
                     'Mix', #transport model
-                    'no',  #is an ARC chemistry ? 'ARC' = yes, other = no
+                    'ARC',  #is an ARC chemistry ? 'ARC' = yes, other = no
                     )
         
 
@@ -77,7 +79,7 @@ if __name__ == '__main__':
     time_formated = time.strftime("%Y%m%d-%H%M%S")
     optimise_mpi_flame_order = False
     species_bg_output = ['CH4','O2','CO','CO2','H2O','N2']
-    
+
     # tolerences for 1D flame solver
     tol_ss = [2.0e-5, 1.0e-9]  # tolerance [rtol atol] for steady-state problem
     tol_ts = [2.0e-5, 1.0e-9]  # tolerance [rtol atol] for time stepping
@@ -97,8 +99,8 @@ if __name__ == '__main__':
             PRINT_MONO_CPU_WARNING()
         else: 
             PRINT_MONO_CPU_WARNING_AND_MPI_NOT_LOADED()
-            for i in range(50):
-                mpiprint('\n WARNING, mpi4py is not installed in your env. Please do not use mpirun -n ...',file=sys.stdout)
+            #for i in range(50):
+            mpiprint('\n WARNING, mpi4py is not installed in your env. Please do not use mpirun -n ...',file=sys.stdout)
                 
 
         MONO_CPU_CALCULATION(items,tol_ss,tol_ts,save_file_name,dim,restart_rate,real_egr,dry,T_reinj,species_bg_output)
