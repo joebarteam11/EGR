50402e0968baa15c8b1821e7a83a5b7abf375e69
Christophe EL HACHEM <elhachem@cerfacs.fr>
2024-03-07 10:44:03 +0100

    Change save of sum up in the csv result folder

--- src/main.py
+++ src/main.py
@@ -40,17 +40,17 @@ if __name__ == '__main__':
         formatted_date = now.strftime("%d_%m_%y_%H-%M-%S")
         print("Results saved on: ", formatted_date)
 
-        templistOx = [300] #[i for i in np.arange(290,305,10.0)]
-        templistFuel = [300] #[i for i in np.arange(290,305,10.0)]
+        templistOx =  [300] #[i for i in np.arange(300,900,100.0)] #[300,400]
+        templistFuel = templistOx #[i for i in np.arange(290,305,10.0)]
         templistEGR = templistOx
         presslist= [1.0E5] #[i for i in np.arange(1E5,1.4E5,0.2E5)]
-        phirange =  [i for i in np.arange(0.400,2.61,0.01)] # [0.85,0.1] # [0.6] [0.6,0.7,0.8,0.9,1.0,1.05,1.1005,1.2005,1.3005,1.4005]#
+        phirange = [i for i in np.arange(0.6,1.5,0.1)] #[1.0,0.89,0.78,0.715,0.66,0.61,0.57,0.53,0.50]    # [i for i in np.arange(0.4,1.2,0.02)] # [0.85,0.1] # [0.6] [0.6,0.7,0.8,0.9,1.0,1.05,1.1005,1.2005,1.3005,1.4005]#
         
         # fuelblendrange = [0.0] if no fuel blend needed
-        fuelblendrange = [0.0]#[i for i in np.arange(0.0,0.301,0.100)] # 
+        fuelblendrange = [0.0] # [0.0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.76] #[i for i in np.arange(0.0,0.301,0.100)] # 
         # egrrange = [0.0] if no dilution needed
         egrrange = [0]#[i for i in np.arange(0.0,0.301,0.1)] 
-        fuels = ['H2:1.0']
+        fuels = ['CH4:1.0']
 
         config = case(fuels,       #fuel compo   e.g. ['CH4:1.0','H2:1.0'] with fuel blend = [0.0] is equivalent to pure CH4 as fuel, with fuel blend = [0.1] is equivalent to 90% CH4 and 10% H2
                     templistFuel,                 #tin fuel
@@ -66,8 +66,8 @@ if __name__ == '__main__':
                     egrrange,                     #[i for i in np.linspace(0.0,0.6,30)],#[0.0,0.1,0.15,0.2],            #egr range
                     'mole',                       #fuelblend rate unit mole / mass
                     'mole',                       #egr rate unit mole / mass
-                    './schemes/H2-SanDiego-Modified-Chemilum.yaml',  #fuel mechanism
-                    #'gri30.yaml',
+                    # './schemes/SanDiego-CH4H2.yaml',  #fuel mechanism
+                    'gri30.yaml',
                     'Mix', #transport model
                     'no',  #is an ARC chemistry ? 'ARC' = yes, other = no
                     saveCSV=True, # Save csv result ? 
@@ -99,13 +99,20 @@ if __name__ == '__main__':
         with open(config_file_path, "w") as file:
             file.write(config_info)
 
+        saveCSVpath = config.saveCSVpath
     else:
         items=[]
+        saveCSVpath = None
+
 
     dim='1D'
-    time_formated = time.strftime("%Y%m%d-%H%M%S")
     optimise_mpi_flame_order = False
-    species_bg_output = ["H2", "H", "O2", "OH", "O", "H2O", "HO2", "H2O2", "N2", "OH*"]
+    species_bg_output = ["H2", "H", "O2", "OH", "O", "H2O", "HO2", "H2O2", "N2"]
+
+    if ncpu != 1:
+        if MPI_LOADED:
+            saveCSVpath = comm.bcast(saveCSVpath,root=0)
+
 
     # tolerences for 1D flame solver
     tol_ss = [2.0e-5, 1.0e-9]  # tolerance [rtol atol] for steady-state problem
@@ -117,9 +124,9 @@ if __name__ == '__main__':
 
     restart_rate = None
     if (real_egr):
-        save_file_name = path + "/results/" + dim + "M12_N2CO2H2OH2ReacOnly" + ".csv"
+        save_file_name = saveCSVpath+"/../sum_up_results_real_egr.csv"
     else:
-        save_file_name = path + "/results/" + dim + "CO2_AP" + ".csv"
+        save_file_name = saveCSVpath+"/../sum_up_results.csv"
 
     if ncpu==1:
         if MPI_LOADED:
